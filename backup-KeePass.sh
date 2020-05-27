7d3bfbeb873249685b7e000b207894f3f762bd38
dss4f <danny@dannyshih.net>
2020-05-27 00:50:39 -0700

    Compress the backup

--- backup-KeePass.sh
+++ backup-KeePass.sh
@@ -45,6 +45,9 @@ function copyIfChanged {
             newFile="$backupDir/$newFileName"
             cp $monitorFile $newFile
 
+            # Compress it
+            gzip -9 $newFile
+
             numBackups=`cat $stateFile | jq -r ".numBackups"`
             writeStateFile $newFileName $(($numBackups + 1))
 
